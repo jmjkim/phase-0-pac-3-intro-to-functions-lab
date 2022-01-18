4d9c7efe16c33b68e53e5919fb2b79b3c432c2df
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-01-18 01:55:26 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+shout("hello");
+
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+whisper("hello");
+
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+logShout("hello");
+
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper("hello");
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    }
+
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+
+    if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+}
+sayHiToHeadphonedRoommate("Let's have dinner together!");
